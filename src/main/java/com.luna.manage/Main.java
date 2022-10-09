d665252493917e57e8ef68a51ecc6935e2e44468
wangzhiwei <517828458@qq.com>
2022-10-09 09:29:06 +0800

    basic commit

---
+++ src/main/java/com.luna.manage/Main.java
@@ -0,0 +1,11 @@
+package com.luna.manage;
+
+/**
+ * @author Artest Wang
+ * Created on 2022/10/9 9:28:31
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A maven project");
+    }
+}
